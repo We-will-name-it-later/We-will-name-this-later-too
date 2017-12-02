656a3a56428243e37dfe6490f7c015458cba4e82
grumpypenny <ajitesh.misra.14@gmail.com>
2017-12-02 12:27:51 -0500

    Added Health Script
    description

---
+++ LD40/Assets/Custom Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+
+	private int currentHealth;
+
+	private void Start()
+	{
+		currentHealth = startingHealth;
+	}
+
+	public void TakeDamage(int damage) {
+		currentHealth -= damage;
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+
+	private void Die() {
+
+	}
+}
