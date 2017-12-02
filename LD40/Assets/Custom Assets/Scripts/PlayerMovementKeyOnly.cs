ed6f7add3232dcb7ffd40140910160f5564b2d7c
grumpypenny <ajitesh.misra.14@gmail.com>
2017-12-02 00:37:29 -0500

    Added Keyboard only version of movement scripts

--- LD40/Assets/Custom Assets/Scripts/PlayerMovementKeyOnly.cs
+++ LD40/Assets/Custom Assets/Scripts/PlayerMovementKeyOnly.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovementKeyOnly : MonoBehaviour {
 
+	public float moveSpeed = 7f;
+
+	private Rigidbody2D rb;
 	private BoxCollider2D box;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		this.rb.MovePosition((Vector2)this.transform.position + input.normalized * moveSpeed * Time.deltaTime);
 	}
 }
