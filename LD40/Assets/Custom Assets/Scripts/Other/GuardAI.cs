ff7b9aab048a64cfa524a68e1b19c23a5e531064
grumpypenny <ajitesh.misra.14@gmail.com>
2017-12-02 17:59:12 -0500

    Guards Now Follow Paths
    Once they see the character, they shoot

--- LD40/Assets/Custom Assets/Scripts/Other/GuardAI.cs
+++ LD40/Assets/Custom Assets/Scripts/Other/GuardAI.cs
@@ -13,9 +13,21 @@ public class GuardAI : MonoBehaviour {
 	public Transform target;
 	private float timeTillNextShot = 0;
 
+	public float speed = 5;
+	public float waitTime = .3f;
+
+	public Transform pathHolder;
+
 	// Use this for initialization
 	void Start () {
 		target = FindObjectOfType<PlayerMovementMouse>().transform;
+		Vector2[] waypoints = new Vector2[pathHolder.childCount];
+		for (int i = 0; i < waypoints.Length; i++)
+		{
+			waypoints[i] = pathHolder.GetChild(i).position;
+		}
+
+		StartCoroutine(FollowPath(waypoints));
 	}
 	
 	// Update is called once per frame
@@ -47,5 +59,54 @@ public class GuardAI : MonoBehaviour {
 			timeTillNextShot += timeBetweenShots;
 			Destroy(Instantiate(Bullet, muzzle.position, transform.rotation), 5);
 		}
+	}
+
+	IEnumerator FollowPath(Vector2[] waypoints)
+	{
+		transform.position = waypoints[0];
+
+		int targetWaypointIndex = 1;
+		Vector2 targetWaypoint = waypoints[targetWaypointIndex];
+		//transform.LookAt(targetWaypoint);
+
+		while (true && !canSeeTarget)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+			if ((Vector2)transform.position == targetWaypoint)
+			{
+				targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+				targetWaypoint = waypoints[targetWaypointIndex];
+				yield return new WaitForSeconds(waitTime);
+				yield return StartCoroutine(TurnToFace(targetWaypoint));
+			}
+			yield return null;
+		}
+	}
+
+	IEnumerator TurnToFace(Vector2 lookTarget)
+	{
+		Vector2 dirToLookTarget = (lookTarget - (Vector2)transform.position).normalized;
+		float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.x, dirToLookTarget.y) * Mathf.Rad2Deg;
+
+		while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle)) > 0.05f)
+		{
+			float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, turnSpeed * Time.deltaTime);
+			transform.eulerAngles = Vector3.forward * angle;
+			yield return null;
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Vector2 startPosition = pathHolder.GetChild(0).position;
+		Vector2 previousPosition = startPosition;
+
+		foreach (Transform waypoint in pathHolder)
+		{
+			Gizmos.DrawSphere(waypoint.position, .3f);
+			Gizmos.DrawLine(previousPosition, waypoint.position);
+			previousPosition = waypoint.position;
+		}
+		Gizmos.DrawLine(previousPosition, startPosition);
 	}
 }
