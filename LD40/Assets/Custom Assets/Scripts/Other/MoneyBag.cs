b7f6d3a2fe04fe74da6d5e4ec470046580b15a1c
grumpypenny <ajitesh.misra.14@gmail.com>
2017-12-04 10:30:38 -0500

    Added Blue flash and Road Sprite

--- LD40/Assets/Custom Assets/Scripts/Other/MoneyBag.cs
+++ LD40/Assets/Custom Assets/Scripts/Other/MoneyBag.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Animations;
 
 public class MoneyBag : Interactable {
 
 	private PlayerMovementMouse player;
 	private ScoreManager scoreManager;
+    private Animator anim;
 
 	private void Start()
 	{
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementMouse>();
 		scoreManager = FindObjectOfType<ScoreManager>();
-	}
+        anim = GameObject.FindGameObjectWithTag("Screen").GetComponent<Animator>();
+    }
 
 	public override void Interact() {
 		base.Interact();
@@ -28,5 +29,6 @@ public class MoneyBag : Interactable {
 		base.Sink();
 		Destroy(this.gameObject);
 		scoreManager.GotBag();
+        anim.SetTrigger("MoneyTrigger");
 	}
 }
