09200e4efd7d37d5849b580f5636a8d75f6ace50
Sharven <sharvenprasad101@gmail.com>
2017-12-04 18:46:42 -0500

    Finished Title Screen

--- LD40/Assets/Custom Assets/Scripts/Other/ButtonManager.cs
+++ LD40/Assets/Custom Assets/Scripts/Other/ButtonManager.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class SceneChange : MonoBehaviour {
+public class ButtonManager : MonoBehaviour {
 
 	public void ChangeScene(int index)
 	{
 		SceneManager.LoadScene(index);
+	}
+
+	public void QuitGame ()
+	{
+		print("Quit!");
+		Application.Quit();
 	}
 }
