a5797be9df5f226e5c411069350f185a88308b51
Sharven <sharvenprasad101@gmail.com>
2017-12-04 00:28:05 -0500

    Added a title screen.
    Made a new title screen scene. It is in the _Scenes folder.

---
+++ LD40/Assets/Custom Assets/Scripts/Other/SceneChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour {
+
+	public void ChangeScene(int index)
+	{
+		SceneManager.LoadScene(index);
+	}
+}
---
+++ LD40/Assets/Custom Assets/Scripts/TitleScreen/RoadScrollMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadScrollMovement : MonoBehaviour {
+
+	public float scrollSpeed;
+
+	// Update is called once per frame
+	private void Update () {
+
+		Vector2 offSet = new Vector2(Time.time * scrollSpeed, 0);
+
+		gameObject.GetComponent<Renderer>().material.mainTextureOffset = offSet;
+		
+	}
+}
